8f91da1a185281ea49495a717b5a23204382af60
Jeremías <jrmsdev@gmail.com>
2017-12-19 21:49:24 -0300

    basic MainActivity.java

---
+++ android/jcmob/app/src/main/java/org/jrmsdev/jcmob/MainActivity.java
@@ -0,0 +1,12 @@
+package org.jrmsdev.jcmob;
+
+import android.app.Activity;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate (Bundle b) {
+        super.onCreate (b);
+    }
+}
