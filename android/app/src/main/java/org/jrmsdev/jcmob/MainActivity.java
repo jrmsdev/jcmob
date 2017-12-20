cd701695536c7900889b41d60df0eac2971883c2
Jeremías <jrmsdev@gmail.com>
2017-12-20 03:56:03 -0300

    webview settings and error handling

--- android/app/src/main/java/org/jrmsdev/jcmob/MainActivity.java
+++ android/app/src/main/java/org/jrmsdev/jcmob/MainActivity.java
@@ -4,17 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 
 // external browser
-import android.net.Uri;
-import android.content.Intent;
+//~ import android.net.Uri;
+//~ import android.content.Intent;
 
 // webview
 import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceError;
+import android.widget.Toast;
 
 import go.jcmob.Jcmob;
 
 public class MainActivity extends Activity {
 
-    private WebView wv;
+    private boolean ENABLE_JS = false;
 
     @Override
     protected void onCreate (Bundle b) {
@@ -38,16 +43,36 @@ public class MainActivity extends Activity {
         super.onDestroy ();
     }
 
-    private void externalBrowserStart () {
-        Uri uri = Uri.parse (Jcmob.start ());
-        Intent intent = new Intent (Intent.ACTION_VIEW, uri);
-        startActivity (intent);
-    }
+    //~ private void externalBrowserStart () {
+        //~ Uri uri = Uri.parse (Jcmob.start ());
+        //~ Intent intent = new Intent (Intent.ACTION_VIEW, uri);
+        //~ startActivity (intent);
+    //~ }
 
     private void webviewResume () {
-        this.wv = new WebView (this);
-        setContentView (this.wv);
-        this.wv.loadData ("<html><body><pre>YEAH!!!</pre></body></html>", "text/html", null);
+        WebView wv = this.newWV ();
+        setContentView (wv);
+        wv.loadUrl (Jcmob.start ());
     }
 
+    private WebView newWV () {
+        WebView wv = new WebView (this);
+        this.wvSettings (wv);
+        return wv;
+    }
+
+    private void wvSettings (WebView wv) {
+        WebSettings ws = wv.getSettings ();
+        ws.setJavaScriptEnabled (this.ENABLE_JS);
+        this.wvClient (wv);
+    }
+
+    private void wvClient (WebView wv) {
+        final Activity activity = this;
+        wv.setWebViewClient (new WebViewClient () {
+            public void onReceivedError (WebView view, WebResourceRequest req, WebResourceError error) {
+                Toast.makeText (activity, error.getDescription (), Toast.LENGTH_SHORT).show ();
+            }
+        });
+    }
 }
