f59835a2663b2361d54b1663f5fba78a220509ca
Jeremías <jrmsdev@gmail.com>
2017-12-20 03:05:09 -0300

    initial webview

--- android/app/src/main/java/org/jrmsdev/jcmob/MainActivity.java
+++ android/app/src/main/java/org/jrmsdev/jcmob/MainActivity.java
@@ -3,23 +3,29 @@ package org.jrmsdev.jcmob;
 import android.app.Activity;
 import android.os.Bundle;
 
-// externalBrowser
+// external browser
 import android.net.Uri;
 import android.content.Intent;
+
+// webview
+import android.webkit.WebView;
 
 import go.jcmob.Jcmob;
 
 public class MainActivity extends Activity {
 
+    private WebView wv;
+
     @Override
     protected void onCreate (Bundle b) {
         super.onCreate (b);
+        //~ this.externalBrowserStart ();
     }
 
     @Override
     protected void onResume () {
         super.onResume ();
-        this.externalBrowser ();
+        this.webviewResume ();
     }
 
     @Override
@@ -32,10 +38,16 @@ public class MainActivity extends Activity {
         super.onDestroy ();
     }
 
-    private void externalBrowser () {
+    private void externalBrowserStart () {
         Uri uri = Uri.parse (Jcmob.start ());
         Intent intent = new Intent (Intent.ACTION_VIEW, uri);
         startActivity (intent);
+    }
+
+    private void webviewResume () {
+        this.wv = new WebView (this);
+        setContentView (this.wv);
+        this.wv.loadData ("<html><body><pre>YEAH!!!</pre></body></html>", "text/html", null);
     }
 
 }
