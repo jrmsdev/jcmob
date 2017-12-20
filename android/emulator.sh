2feda1b1a154042c83f2ba2388f200756af26710
Jeremías <jrmsdev@gmail.com>
2017-12-20 03:04:38 -0300

    use ANDROID_HOME for emulator path

--- android/emulator.sh
+++ android/emulator.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-exec android-emulator @4.65_720p_Galaxy_Nexus_Edited_API_26 \
+exec ${ANDROID_HOME}/emulator/emulator @4.65_720p_Galaxy_Nexus_Edited_API_26 \
 	-gpu off \
 	-logcat 'ActivityManager:e AndroidRuntime:e' \
 	-netfast \
