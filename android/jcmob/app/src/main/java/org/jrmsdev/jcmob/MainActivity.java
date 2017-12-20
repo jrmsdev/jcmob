c43d9b3a4a19cb6f47b81da1a035e0b31f8a3dc0
Jeremías <jrmsdev@gmail.com>
2017-12-19 21:55:38 -0300

    intent action view on resume

--- android/jcmob/app/src/main/java/org/jrmsdev/jcmob/MainActivity.java
+++ android/jcmob/app/src/main/java/org/jrmsdev/jcmob/MainActivity.java
@@ -1,12 +1,33 @@
 package org.jrmsdev.jcmob;
 
 import android.app.Activity;
-import android.app.Activity;
 import android.os.Bundle;
+import android.net.Uri;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate (Bundle b) {
         super.onCreate (b);
     }
+
+    @Override
+    protected void onResume () {
+        super.onResume ();
+        Uri uri = Uri.parse ("https://slashdot.com/");
+        Intent intent = new Intent (Intent.ACTION_VIEW, uri);
+        startActivity (intent);
+    }
+
+    @Override
+    protected void onPause () {
+        super.onPause ();
+    }
+
+    @Override
+    protected void onDestroy () {
+        super.onDestroy ();
+    }
+
 }
