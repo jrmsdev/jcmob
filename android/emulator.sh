8c3a6b68476a8458a8c6cc8d47d78373239becf4
Jeremías <jrmsdev@gmail.com>
2017-12-20 04:17:17 -0300

    emulator args

--- android/emulator.sh
+++ android/emulator.sh
@@ -4,4 +4,4 @@ exec ${ANDROID_HOME}/emulator/emulator @4.65_720p_Galaxy_Nexus_Edited_API_26 \
 	-logcat 'ActivityManager:e,AndroidRuntime:e' \
 	-netfast \
 	-accel on \
-	-no-boot-anim
+	-no-boot-anim $@
