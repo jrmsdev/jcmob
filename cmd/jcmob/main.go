690e1851c823cec782da589e5a20ccea52e91413
Jeremías <jrmsdev@gmail.com>
2017-12-21 17:36:37 -0300

    use jcms/cli

--- cmd/jcmob/main.go
+++ cmd/jcmob/main.go
@@ -1,23 +1,9 @@
 package main
 
 import (
-    "github.com/zserge/webview"
-    "github.com/jrmsdev/jcmob/jcmob"
+    "github.com/jrmsdev/go-jcms/jcms/cli"
 )
 
 func main () {
-    println ("listen")
-    uri := jcmob.Listen ()
-    println ("listen uri:", uri)
-
-    println ("serve")
-    go func() {
-        jcmob.Serve ()
-    }()
-
-    resize := true
-    webview.Open ("JCMobile", uri, 800, 600, resize)
-
-    println ("stop")
-    jcmob.Stop ()
+    cli.Webview ()
 }
