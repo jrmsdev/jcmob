bf262a74019d81b3e132ad56f07610e908c47951
Jeremías <jrmsdev@gmail.com>
2018-11-22 16:17:15 -0300

    vendor golang.org/x/mobile as git submodule

--- gomobile-setup.sh
+++ gomobile-setup.sh
@@ -1,18 +1,17 @@
 #!/bin/sh -eux
 # ref: https://github.com/mpl/go4droid/blob/master/Dockerfile
 
-export GOPATH=/opt/golang
-export ANDROID_HOME=/opt/android/sdk
+#export ANDROID_HOME=/opt/android/sdk
 
-go get -d -u golang.org/x/mobile/cmd/gomobile
-go clean -i golang.org/x/mobile/cmd/gomobile
-go install golang.org/x/mobile/cmd/gomobile
+#go get -d -u golang.org/x/mobile/cmd/gomobile
+go clean -i ./vendor/golang.org/x/mobile/cmd/gomobile
+go install -i ./vendor/golang.org/x/mobile/cmd/gomobile
 
-go get -d -u golang.org/x/mobile/cmd/gobind
-go clean -i golang.org/x/mobile/cmd/gobind
-go install golang.org/x/mobile/cmd/gobind
+#go get -d -u golang.org/x/mobile/cmd/gobind
+go clean -i ./vendor/golang.org/x/mobile/cmd/gobind
+go install -i ./vendor/golang.org/x/mobile/cmd/gobind
 
-gomobile clean
-gomobile init -ndk $ANDROID_HOME/ndk-bundle
+gomobile clean || true
+echo gomobile init -ndk $ANDROID_HOME/ndk-bundle
 
 exit 0
