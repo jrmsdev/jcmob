d41bbed81dfac2ca3a257d64b60e81eabaa42765
Jeremías <jrmsdev@gmail.com>
2018-11-22 01:04:07 -0300

    fake asset file stat info

--- asset_manager.go
+++ asset_manager.go
@@ -3,7 +3,7 @@ package jcmob
 import (
 	"os"
 	"io/ioutil"
-	"path/filepath"
+	"time"
 
 	"github.com/jrmsdev/go-jcms/lib/jcms/api"
 	"golang.org/x/mobile/asset"
@@ -20,8 +20,7 @@ func (m *assetManager) Open(filename string) (api.AssetFile, error) {
 }
 
 func (m *assetManager) Stat(filename string) (os.FileInfo, error) {
-	fn := filepath.Join("assets", filename)
-	return os.Stat(fn)
+	return newFileInfo(filename)
 }
 
 func (m *assetManager) ReadFile(name string) ([]byte, error) {
@@ -31,4 +30,53 @@ func (m *assetManager) ReadFile(name string) ([]byte, error) {
 	}
 	defer fh.Close()
 	return ioutil.ReadAll(fh)
+}
+
+var modTime time.Time
+
+func init() {
+	modTime = time.Now()
+}
+
+type fileInfo struct{
+	name string
+	size int64
+}
+
+func newFileInfo(name string) (*fileInfo, error) {
+	fh, err := asset.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	buf, readErr := ioutil.ReadAll(fh)
+	if readErr != nil {
+		return nil, readErr
+	}
+	sz := int64(len(buf))
+	buf = nil
+	return &fileInfo{name, sz}, nil
+}
+
+func (i *fileInfo) Name() string {
+	return i.name
+}
+
+func (i *fileInfo) Size() int64 {
+	return i.size
+}
+
+func (i *fileInfo) Mode() os.FileMode {
+	return os.ModeType
+}
+
+func (i *fileInfo) ModTime() time.Time {
+	return modTime
+}
+
+func (i *fileInfo) IsDir() bool {
+	return false
+}
+
+func (i *fileInfo) Sys() interface{} {
+	return nil
 }
